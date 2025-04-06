7ce5e5538aa7c4ece8731dd53555d2c3bd93469a
DanKazh <117713084+DanKazh@users.noreply.github.com>
2025-04-06 19:50:45 +0300

    Update urls.py

--- ask_kazhenets/urls.py
+++ ask_kazhenets/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('hot/', views.hot, name='hot'),
-    path('tag/<str:tag_name>/', views.tag, name='tag'),
-    path('question/<int:question_id>/', views.question, name='question'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('ask/', views.ask, name='ask'),
-    path('usersettings/', views.user_settings, name='usersettings'),
+    path('', include('app.urls')),
 ]
