b60fa1f938e8ebe2e7fdea3de8df842eeae46ea9
DanKazh <117713084+DanKazh@users.noreply.github.com>
2025-05-28 08:49:53 +0300

    Add files via upload

---
+++ dz/ask_kazhenets/ask_kazhenets/gunicorn_config.py
@@ -0,0 +1,12 @@
+import os
+
+bind = "0.0.0.0:8000"
+workers = 2
+worker_class = "sync"
+timeout = 120
+max_requests = 500
+max_requests_jitter = 50
+
+
+pythonpath = '/home/sapbash/projects/ask_kazhenets'
+chdir = '/home/sapbash/projects/ask_kazhenets'
